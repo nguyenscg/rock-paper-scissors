33d1a586c2d64a79a65b3119842e4b63e84076cf
nguyenscg <nguyencsg@gmail.com>
2024-09-27 16:53:34 -0500

    added let's play message and prompt input function. imported random module

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -1,3 +1,6 @@
+# import random module
+import random
+
 rock = '''
     _______
 ---'   ____)
@@ -24,3 +27,14 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+
+# 0 = rock, 1 = paper, # 2 = scissors
+
+# how to decide if someone were to win or lose?
+
+# how to choose random shape
+print("Let's play Rock, Paper, Scissors!")
+user_input = input("What do you choose? 0 for Rock, 1 for Paper, 2 for Scissors")
+
+# random number from 0 to 2, include 2
+random_num = random.randint(0, 2)
